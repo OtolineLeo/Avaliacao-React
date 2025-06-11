6e06d0b429b1af41165f765ab620a4652a868404
unknown <carlosotoline@gmail.com>
2025-06-11 17:29:30 -0300

    adicionando as tarefas

--- src/pages/tarefas/tarefas.jsx
+++ src/pages/tarefas/tarefas.jsx
@@ -1,9 +1,10 @@
-import { Button } from "../../components/Button/Button"
-import style from "./tarefas.module.css"
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../components/Button/Button";
+import styles from "./tarefas.module.css";
+import { Input } from "../../components/Input/input"
 
 export function TarefasPage() {
-
   const [texto, setTexto] = useState('');
     const [itens, setItens] = useState([]);
 
@@ -24,45 +25,51 @@ export function TarefasPage() {
         setTexto('')
     };
 
-    return(
-        <>
-        <Navbar/>
-            <div className={style.tarefasEdit}>
-                <h1>Tarefas</h1>
+    const navigate = useNavigate(); 
 
+  return(
+      <>        
+        <div className={styles.tarefasEdit}>
+          <h1>Tarefas</h1>
 
+         <div className={styles.caixaComScroll}>
+            {itens.map((item, index) => (
 
-            <div class={style.caixaComScroll}>
-                {itens.map((item, index) => (
+                <p key={index}>
+                  {item}
+                  </p>
 
-                    <p key={index}>
-                        {item}
-                    </p>
-
-                ))}
-            </div>
+            ))}
+          </div>
 
             <Input
                 type="text"
                 placeholder="Digite sua tarefa..."
                 onChange={(e) => setTexto(e.target.value)}
-                value={""}
+                value={texto}
                 onKeyPress={handleKeyPress}
             />
 
-            <div className={style.buttonEditavel}>
+            <div className={styles.buttonEditavel}>
             <Button
                 type="submit"
                 onClick={adicionarTexto}
-                title={'Adicionar'}
+                message={'Adicionar'}
+                
             />
             <Button
                 type="submit"
                 onClick={handleLimpar}
-                title={'Limpar  tudo'}
+                message={'Limpar  tudo'}
+            />
+            <Button
+                type="button"
+                message="Voltar para Home"
+                onClick={() => navigate("/")}
+                
             />
            </div>
         </div>
         </>
     )
-}
+} 
